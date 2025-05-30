5593a684021dc52874987f5c7f1fe576f5b725df
Melody Renata <me@lovingmelody.io>
2025-05-30 06:27:45 -0400

    Nvidia: Fast track driver update

--- modules/nixos/nvidia/default.nix
+++ modules/nixos/nvidia/default.nix
@@ -25,18 +25,29 @@ in {
     };
     hardware = {
       nvidia = {
-        package = mkDefault ((patchLibcuda config.boot.kernelPackages.nvidiaPackages.beta).overrideAttrs (o: {
-          patches =
-            (o.patches or [])
-            ++ lib.optional (o.version == "575.51.02")
-            (pkgs.fetchpatch
-              {
-                url = "https://github.com/CachyOS/kernel-patches/raw/914aea4298e3744beddad09f3d2773d71839b182/6.15/misc/nvidia/0003-Workaround-nv_vm_flags_-calling-GPL-only-code.patch";
-                hash = "sha256-YOTAvONchPPSVDP9eJ9236pAPtxYK5nAePNtm2dlvb4=";
-                stripLen = 1;
-                extraPrefix = "kernel/";
-              });
-        }));
+        package = mkDefault (patchLibcuda (
+          if lib.versionAtLeast "575.07.08" config.boot.kernelPackages.nvidiaPackages.beta.version
+          then builtins.trace "Uneeded mkDriver in modules/nixos/nvidia/default.nix" config.boot.kernelPackages.nvidiaPackages.beta
+          else
+            config.boot.kernelPackages.nvidiaPackages.mkDriver {
+              version = "575.57.08";
+              sha256_64bit = "sha256-KqcB2sGAp7IKbleMzNkB3tjUTlfWBYDwj50o3R//xvI=";
+              sha256_aarch64 = "sha256-VJ5z5PdAL2YnXuZltuOirl179XKWt0O4JNcT8gUgO98=";
+              openSha256 = "sha256-DOJw73sjhQoy+5R0GHGnUddE6xaXb/z/Ihq3BKBf+lg=";
+              settingsSha256 = "sha256-AIeeDXFEo9VEKCgXnY3QvrW5iWZeIVg4LBCeRtMs5Io=";
+              persistencedSha256 = "sha256-Len7Va4HYp5r3wMpAhL4VsPu5S0JOshPFywbO7vYnGo=";
+
+              patches = [
+                (pkgs.fetchpatch
+                  {
+                    url = "https://github.com/CachyOS/kernel-patches/raw/914aea4298e3744beddad09f3d2773d71839b182/6.15/misc/nvidia/0003-Workaround-nv_vm_flags_-calling-GPL-only-code.patch";
+                    hash = "sha256-YOTAvONchPPSVDP9eJ9236pAPtxYK5nAePNtm2dlvb4=";
+                    stripLen = 1;
+                    extraPrefix = "kernel/";
+                  })
+              ];
+            }
+        ));
         modesetting.enable = mkDefault true;
         nvidiaSettings = mkDefault true;
         powerManagement.enable = mkDefault true;
