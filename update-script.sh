be12de6146d14ae5d6b81078663b26e3cddab112
Melody Renata <me@lovingmelody.io>
2025-06-25 19:52:20 -0400

    chore: style

--- update-script.sh
+++ update-script.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 # Note: flake inputs should be run before running this
-MC_VERSION=$(nix eval .\#packages.x86_64-linux.minecraftServers.fabric-1_21_2.version --raw)
-echo "Updating MC sources for $MC_VERSION"
+echo "Updating MC sources"
 python3 ./modules/nixos/minecraft-server/generate-sources.py "$MC_VERSION" >./modules/nixos/minecraft-server/sources.json
 
 echo "Updating npins"
