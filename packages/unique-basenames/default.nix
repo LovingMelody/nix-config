186e73e64fa7cca0c7327c33756d2d776816eb2c
Melody Renata <me@lovingmelody.io>
2025-07-03 18:57:05 -0400

    unique-basenames: fix

--- packages/unique-basenames/default.nix
+++ packages/unique-basenames/default.nix
@@ -4,6 +4,8 @@ with python3Packages;
     name = "unique-basenames";
     version = "1.0";
     src = ./src;
+    pyproject = true;
+    build-system = [setuptools];
     meta = {
       description = "Script to take a list of paths and deduplicate using the basename";
       mainProgram = "unique-basenames";
