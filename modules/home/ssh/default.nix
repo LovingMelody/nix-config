99fa9c3fa2bad92451be4986cc8596d8dd530bdb
Melody Renata <me@lovingmelody.io>
2025-08-27 07:35:39 -0400

    shh compression: set to true

--- modules/home/ssh/default.nix
+++ modules/home/ssh/default.nix
@@ -11,7 +11,7 @@ in {
   config = mkIf cfg.enable {
     programs.ssh = {
       enable = true;
-      compression = mkDefault true;
+      compression = true;
       forwardAgent = true;
       matchBlocks = {
         "eu.nixbuild.net" = {
