9ed3a1324812e9ec5502f8f178e2003e395c9704
Melody Renata <me@lovingmelody.io>
2025-10-04 22:54:26 -0400

    Update script: use nix-shell

--- update-script.sh
+++ update-script.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env bash
+#!/usr/bin/env nix-shell
+#! nix-shell -i bash
+#! nix-shell -p bash nix-update npins
 # Note: flake inputs should be run before running this
 echo "Updating MC sources"
 python3 ./modules/nixos/minecraft-server/generate-sources.py >./modules/nixos/minecraft-server/sources.json
