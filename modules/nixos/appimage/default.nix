9a0851a61392f3005df6ad44cf3e4309db7ba4dc
Melody Renata <me@lovingmelody.io>
2025-10-28 13:16:08 -0400

    appimage (nixos): init

---
+++ modules/nixos/appimage/default.nix
@@ -0,0 +1,30 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  inherit (lib) mkEnableOption mkOption mkIf;
+  cfg = config.TM.programs.appimage;
+in {
+  options.TM.programs.appimage = {
+    enable = mkEnableOption "Enable Appimage & binfmt" // {default = config.TM.isDesktop;};
+    extraPkgs = mkOption {
+      description = "Extra packages for appimage-run";
+      default = _pkgs: [];
+    };
+    package = mkOption {
+      description = "App image runner package";
+      default = pkgs.appimage-run;
+      apply = p: p.override (o: {extraPkgs = pkgs: ((o.extraPkgs or (_: [])) pkgs) ++ (cfg.extraPkgs pkgs);});
+    };
+  };
+
+  config = mkIf cfg.enable {
+    programs.appimage = {
+      enable = true;
+      binfmt.enable = true;
+      inherit (cfg) package;
+    };
+  };
+}
