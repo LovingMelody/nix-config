a07eea7e7da3248f5b2e5fd75d82f2f17f638bb5
Melody Renata <me@lovingmelody.io>
2025-11-27 17:02:38 -0500

    networkmanager: Update nameservers

--- modules/nixos/defaults/default.nix
+++ modules/nixos/defaults/default.nix
@@ -201,6 +201,16 @@ in
                 then "systemd-resolved"
                 else "default"
               );
+              insertNameservers = [
+                "1.1.1.1"
+                "1.0.0.1"
+                "2606:4700:4700::1111"
+                "2606:4700:4700::1001"
+                "9.9.9.9"
+                "149.112.112.112"
+                "2620:fe::fe"
+                "2620:fe::9"
+              ];
               wifi.powersave = mkDefault config.TM.isLaptop;
             };
           });
