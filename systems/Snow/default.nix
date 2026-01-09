98db90a17866d5704a48013bad9f4aa74adc1305
Melody Renata <me@lovingmelody.io>
2026-01-08 22:21:44 -0500

    Snow: Patch kernel to fix SteamVR

--- systems/Snow/default.nix
+++ systems/Snow/default.nix
@@ -264,4 +264,14 @@ in {
   };
 
   boot.kernel.sysctl."vm.max_map_count" = lib.mkForce 16777216;
+  boot.kernelPatches = [
+    {
+      name = "amdgpu-ignore-ctx-privileges";
+      patch = pkgs.fetchpatch {
+        name = "cap_sys_nice_begone.patch";
+        url = "https://github.com/Frogging-Family/community-patches/raw/master/linux61-tkg/cap_sys_nice_begone.mypatch";
+        hash = "sha256-Y3a0+x2xvHsfLax/uwycdJf3xLxvVfkfDVqjkxNaYEo=";
+      };
+    }
+  ];
 }
