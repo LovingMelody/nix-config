7de61c00fc0259372f6047bd38e9ca3f6c519062
Melody Renata <me@lovingmelody.io>
2026-03-19 22:55:34 -0400

    Vulpix: Switch to KDE

--- systems/Vulpix/default.nix
+++ systems/Vulpix/default.nix
@@ -53,13 +53,21 @@ in {
     applications = mkForce 12;
     popups = mkForce 10;
   };
-  services.desktopManager.cosmic.enable = true;
+  services = {
+    desktopManager.cosmic.enable = true;
+
+    desktopManager.plasma6.enable = true;
+    displayManager.sddm = {
+      enable = true;
+      wayland.enable = true;
+    };
+  };
   programs.opengamepadui = {
     enable = true;
     inputplumber.enable = true;
   };
   # services.displayManager.cosmic-greeter.enable = true;
-  TM.desktop.gnome.enable = true;
+  TM.desktop.gnome.enable = false;
   # specialisation = {
   #   # Configs has conflits w/ gnome & wayland doesnt run the best w/ nvidia
   #   gnome = {
@@ -134,6 +142,22 @@ in {
     nil
     android-tools
     # yubioath-flutter
+    #KDE
+    kdePackages.discover # Optional: Install if you use Flatpak or fwupd firmware update sevice
+    kdePackages.kcalc # Calculator
+    kdePackages.kcharselect # Tool to select and copy special characters from all installed fonts
+    kdePackages.kclock # Clock app
+    kdePackages.kcolorchooser # A small utility to select a color
+    kdePackages.kolourpaint # Easy-to-use paint program
+    kdePackages.ksystemlog # KDE SystemLog Application
+    kdePackages.sddm-kcm # Configuration module for SDDM
+    kdiff3 # Compares and merges 2 or 3 files or directories
+    kdePackages.isoimagewriter # Optional: Program to write hybrid ISO files onto USB disks
+    kdePackages.partitionmanager # Optional: Manage the disk devices, partitions and file systems on your computer
+    # Non-KDE graphical packages
+    hardinfo2 # System information and benchmarks for Linux systems
+    wayland-utils # Wayland utilities
+    wl-clipboard # Command-line copy/paste utilities for Wayland
   ];
 
   security.polkit.enable = true;
