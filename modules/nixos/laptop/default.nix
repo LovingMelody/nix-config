94e6e4e5f1b87aae5a93a167dd6aba54a1fb04d1
Melody Renata <me@lovingmelody.io>
2026-04-08 18:24:57 -0400

    Update laptop config

--- modules/nixos/laptop/default.nix
+++ modules/nixos/laptop/default.nix
@@ -5,21 +5,28 @@
 }: let
   # Defined in helper
   cfg = config.TM.isLaptop;
-  inherit (lib) mkDefault mkIf;
+  inherit (lib) mkIf;
 in {
   config = mkIf cfg {
-    services.thermald.enable = mkDefault true;
-    services.tlp.settings = {
-      CPU_BOOST_ON_AC = 1;
-      CPU_BOOST_ON_BAT = 0;
-      CPU_HWP_DYN_BOOST_ON_AC = 1;
-      CPU_HWP_DYN_BOOST_ON_BAT = 0;
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-      PLATFORM_PROFILE_ON_AC = "performance";
-      PLATFORM_PROFILE_ON_BAT = "low-power";
+    services = {
+      thermald.enable = true;
+      tlp = {
+        enable = true;
+        pd.enable = true;
+        settings = {
+          CPU_BOOST_ON_AC = 1;
+          CPU_BOOST_ON_BAT = 0;
+          CPU_HWP_DYN_BOOST_ON_AC = 1;
+          CPU_HWP_DYN_BOOST_ON_BAT = 0;
+          CPU_SCALING_GOVERNOR_ON_AC = "performance";
+          CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+          CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+          CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+          PLATFORM_PROFILE_ON_AC = "performance";
+          PLATFORM_PROFILE_ON_BAT = "low-power";
+        };
+      };
+      power-profiles-daemon.enable = false;
     };
   };
 }
