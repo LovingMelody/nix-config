327b43e8717eca2b90b6e185e7acfae2e34a876a
Melody Renata <me@lovingmelody.io>
2026-05-03 04:56:43 -0400

    flake.lock: Update
    Flake lock file updates:
    
    • Added input 'lix':
        'https://git.lix.systems/api/v1/repos/lix-project/lix/archive/cd573beb0a60d6bf0e341bcd4d2c464ed97c1a8a.tar.gz?narHash=sha256-VcDmXhzxxEiVzXj5f3vOpAZKkyGIvHYy4rPZxr02RYA%3D' (2026-05-01)
    • Added input 'lix-module':
        'https://git.lix.systems/api/v1/repos/lix-project/nixos-module/archive/5e56f5a973e24292b125dca9e9d506b0a91d6903.tar.gz?narHash=sha256-y9kC3ff89btXS8RD6pAtM50g0qtsim1I8HXBtgSqdbI%3D' (2026-03-14)
    • Added input 'lix-module/flake-utils':
        follows 'flake-utils'
    • Added input 'lix-module/flakey-profile':
        'github:lf-/flakey-profile/243c903fd8eadc0f63d205665a92d4df91d42d9d?narHash=sha256-FhGIEU93VHAChKEXx905TSiPZKga69bWl1VB37FK//I%3D' (2024-04-12)
    • Added input 'lix-module/lix':
        follows 'lix'
    • Added input 'lix-module/nixpkgs':
        follows 'nixpkgs'

--- flake.nix
+++ flake.nix
@@ -239,6 +239,19 @@
         nixpkgs-lib.follows = "nixpkgs";
       };
     };
+    lix = {
+      url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
+      flake = false;
+    };
+
+    lix-module = {
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        lix.follows = "lix";
+        flake-utils.follows = "flake-utils";
+      };
+    };
     niri = {
       url = "github:YaLTeR/niri";
       inputs = {
