cd52c2e79e1d2fdbdffde383a3429f1cd998da0e
Melody Renata <me@lovingmelody.io>
2026-05-06 20:14:54 -0400

    Add fonts

--- modules/nixos/styles/default.nix
+++ modules/nixos/styles/default.nix
@@ -8,61 +8,80 @@
   inherit
     (lib)
     mkDefault
+    mkMerge
     mkIf
     ;
-  inherit (lib.TM) get-shared-module;
+  inherit (lib.TM) get-shared-module get-secret-file;
 in {
   imports = [(import (get-shared-module "styles") {osConfig = {};})];
   # TODO: Replace stylix some of the enabled styles just dont work
   # Breaks some configs such as waybar by default
 
-  config = mkIf cfg.enable {
-    environment.systemPackages = lib.optionals config.TM.isGui [
-      pkgs.kdePackages.qtstyleplugin-kvantum
-      (pkgs.catppuccin-kde.override {
-        flavour = [config.catppuccin.flavor];
-        accents = [config.catppuccin.accent];
-      })
-    ];
-    programs.dconf.enable = true;
-    boot.plymouth.logo = builtins.fetchurl {
-      url = "https://raw.githubusercontent.com/catppuccin/catppuccin/b3584ebc7a74fed37bd7d0dda494d88c17248439/assets/footers/gray0_ctp_on_line.png";
-      sha256 = "0avvj5mr642v4djkpwym8drgzq4pskh4bdpb5998spplr8ymqq17";
-    };
-    qt = {
-      enable = mkDefault config.TM.isGui;
-      # platformTheme = "kvantum";
-      # style = mkForce "adwaita${lib.strings.optionalString (config.stylix.polarity == "dark") "-dark"}";
-    };
-    fonts = {
-      fontDir.enable = config.TM.isGui;
-      packages = lib.optionals config.TM.isGui [
-        config.stylix.fonts.monospace.package
-        config.stylix.fonts.serif.package
-        pkgs.font-awesome
-        pkgs.inter
-        pkgs.nerd-fonts.caskaydia-cove
-        pkgs.nerd-fonts.caskaydia-mono
-        pkgs.nerd-fonts.open-dyslexic
-        pkgs.nerd-fonts.symbols-only
-        pkgs.noto-fonts-cjk-sans
-        pkgs.noto-fonts-cjk-serif
-        pkgs.open-dyslexic
+  config = mkIf cfg.enable (mkMerge [
+    (mkIf config.TM.knowsHiddenMove {
+      sops.secrets."Fonts/fontT.ttf" = {
+        sopsFile = get-secret-file "fonts/fontT.bin";
+        format = "binary";
+        owner = "root";
+        mode = "444";
+        path = "/usr/local/share/fonts/t/fontT.ttf";
+      };
+      fonts.fontconfig.localConf = ''
+        <?xml version="1.0"?>
+        <!DOCTYPE fontconfig SYSTEM "urn:fontconfig:fonts.dtd">
+        <fontconfig>
+          <dir>/usr/local/share/fonts/t</dir>
+        </fontconfig>
+      '';
+    })
+    {
+      environment.systemPackages = lib.optionals config.TM.isGui [
+        pkgs.kdePackages.qtstyleplugin-kvantum
+        (pkgs.catppuccin-kde.override {
+          flavour = [config.catppuccin.flavor];
+          accents = [config.catppuccin.accent];
+        })
       ];
-      fontconfig = {
-        hinting.style = "medium";
-        useEmbeddedBitmaps = true;
+      programs.dconf.enable = true;
+      boot.plymouth.logo = builtins.fetchurl {
+        url = "https://raw.githubusercontent.com/catppuccin/catppuccin/b3584ebc7a74fed37bd7d0dda494d88c17248439/assets/footers/gray0_ctp_on_line.png";
+        sha256 = "0avvj5mr642v4djkpwym8drgzq4pskh4bdpb5998spplr8ymqq17";
       };
-    };
-    stylix = {
-      homeManagerIntegration.autoImport = false;
-      # Catppuccin themes this
-      targets = {
-        plymouth.enable = false;
-        grub.enable = false;
-        chromium.enable = config.TM.isGui;
-        qt.enable = true;
+      qt = {
+        enable = mkDefault config.TM.isGui;
+        # platformTheme = "kvantum";
+        # style = mkForce "adwaita${lib.strings.optionalString (config.stylix.polarity == "dark") "-dark"}";
       };
-    };
-  };
+      fonts = {
+        fontDir.enable = config.TM.isGui;
+        packages = lib.optionals config.TM.isGui [
+          config.stylix.fonts.monospace.package
+          config.stylix.fonts.serif.package
+          pkgs.font-awesome
+          pkgs.inter
+          pkgs.nerd-fonts.caskaydia-cove
+          pkgs.nerd-fonts.caskaydia-mono
+          pkgs.nerd-fonts.open-dyslexic
+          pkgs.nerd-fonts.symbols-only
+          pkgs.noto-fonts-cjk-sans
+          pkgs.noto-fonts-cjk-serif
+          pkgs.open-dyslexic
+        ];
+        fontconfig = {
+          hinting.style = "medium";
+          useEmbeddedBitmaps = true;
+        };
+      };
+      stylix = {
+        homeManagerIntegration.autoImport = false;
+        # Catppuccin themes this
+        targets = {
+          plymouth.enable = false;
+          grub.enable = false;
+          chromium.enable = config.TM.isGui;
+          qt.enable = true;
+        };
+      };
+    }
+  ]);
 }
