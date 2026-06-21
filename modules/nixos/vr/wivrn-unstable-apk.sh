f910e774903bcb39ad49826b9e2143e141a55d17
Melody Renata <me@lovingmelody.io>
2026-06-21 05:05:16 -0400

    ci: fix wivrn-unstable-apk not running in CI

--- modules/nixos/vr/wivrn-unstable-apk.sh
+++ modules/nixos/vr/wivrn-unstable-apk.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env bash
+#!/usr/bin/env nix-shell
+#! nix-shell -i bash
+#! nix-shell -p bash jaq
 INFO="./modules/nixos/vr/wivrn-apk.json"
 VERSION="$(nix eval .#nixosConfigurations.Snow.pkgs.wivrn.version --raw 2>/dev/null)"
 # Don't needlessly update
@@ -7,5 +9,9 @@ if [ "$(jaq -r .version <"$INFO")" == "$VERSION" ]; then
 fi
 WIVRN_APK_URL="https://github.com/WiVRn/WiVRn-APK/releases/download/apk-$VERSION/org.meumeu.wivrn-release.apk"
 HASH="$(nix store prefetch-file --hash-type sha512 "$WIVRN_APK_URL" --json | jaq -r .hash)"
+if [ "$HASH" == "" ]; then
+  echo "Failed to fetch hash for \`$WIVRN_APK_URL\`"
+  exit 1
+fi
 jaq -n --arg url "$WIVRN_APK_URL" --arg hash "$HASH" --arg version "$VERSION" \
   '{version: $version, hash: $hash, url: $url}' >"$INFO"
