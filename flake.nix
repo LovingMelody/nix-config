ab6c058867448ff62b9bcaad6340e0b593410a65
Melody Renata <me@lovingmelody.io>
2026-06-29 05:37:06 -0400

    Update lix

--- flake.nix
+++ flake.nix
@@ -249,12 +249,12 @@
       };
     };
     lix = {
-      url = "https://git.lix.systems/lix-project/lix/archive/2.95.3.tar.gz";
+      url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
       flake = false;
     };
 
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/1688100bba140492658d597f6b307c327f35c780.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
       inputs = {
         nixpkgs.follows = "nixpkgs";
         lix.follows = "lix";
