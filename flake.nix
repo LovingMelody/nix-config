a70325f4d18c6740746cd971e1255ecef5c41187
Melody Renata <me@lovingmelody.io>
2026-07-16 04:15:38 -0400

    flake: drop garnix.io. Service shut down

--- flake.nix
+++ flake.nix
@@ -164,8 +164,8 @@
     });
   };
   nixConfig = {
-    extra-substituters = ["https://cache.garnix.io"];
-    extra-trusted-public-keys = ["cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="];
+    extra-substituters = [];
+    extra-trusted-public-keys = [];
   };
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
